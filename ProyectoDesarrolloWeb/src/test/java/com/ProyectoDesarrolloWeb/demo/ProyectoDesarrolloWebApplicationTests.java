2fe063ef737a02a4e524af2130e9442d01124e5f
marianaalvarezvillalta <marianaalv07@gmail.com>
2023-02-01 09:59:23 -0600

    prueba

---
+++ ProyectoDesarrolloWeb/src/test/java/com/ProyectoDesarrolloWeb/demo/ProyectoDesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoDesarrolloWeb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
